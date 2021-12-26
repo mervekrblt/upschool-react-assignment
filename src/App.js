ea7df9125bc525c13105ad41b09c6c32f54f573f
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-12-26 21:37:48 +0300

    Change database name to db

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [data, setData] = useState(null)
+  
+  fetch("http://localhost:4000/products")
+  .then(response => response.json())
+  .then(data => setData(data))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {JSON.stringify(data)}
+    </>
   );
 }
 
