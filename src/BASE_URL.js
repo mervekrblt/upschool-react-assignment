c9eb6e609216b89ac399db2ff20723103b5a02ef
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-12-30 01:23:59 +0300

    Change URL

--- src/BASE_URL.js
+++ src/BASE_URL.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://my-json-server.typicode.com/mervekrblt/upschool-react-assignment"
+const BASE_URL = "https://my-json-server.typicode.com/mervekrblt/upschool-react-assignment/products"
 
 export default BASE_URL
